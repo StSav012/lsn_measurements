19a5a039be4c25ea65a0ebf311434c7497c87a0a
stsav012 <stsav012@gmail.com>
2022-05-11 13:23:18 +0300

    Unified GUI for some applications

--- lifetime vs bias gui.py
+++ lifetime vs bias gui.py
@@ -9,10 +9,9 @@ from typing import Dict, Final, List, Optional, TextIO, Tuple
 
 import numpy as np
 import pyqtgraph as pg
-from PyQt5.QtCore import QSettings, QTimer, Qt
-from PyQt5.QtGui import QCloseEvent, QColor, QIcon
-from PyQt5.QtWidgets import (QApplication, QFormLayout, QGroupBox, QHBoxLayout, QMainWindow, QPushButton, QVBoxLayout,
-                             QWidget)
+from PyQt5.QtCore import QTimer, Qt
+from PyQt5.QtGui import QCloseEvent, QColor
+from PyQt5.QtWidgets import QApplication
 from numpy.typing import NDArray
 
 from backend.communication.anapico_communication import APUASYN20
@@ -20,156 +19,14 @@ from backend.communication.triton_communication import Triton
 from backend.measurement.lifetime import LifetimeMeasurement
 from backend.utils import SliceSequence, error, warning, zero_sources
 from backend.utils.config import *
+from ui.lifetime_gui import LifetimeGUI
 
 
-class GUI(QMainWindow):
-    def __init__(self, flags=Qt.WindowFlags()) -> None:
-        super(GUI, self).__init__(flags=flags)
-
-        self.settings: QSettings = QSettings("SavSoft", "Lifetime", self)
-
-        self.setWindowTitle('Lifetime')
-        self.setWindowIcon(QIcon('lsn.svg'))
-
-        self.central_widget: QWidget = QWidget(self)
-        self.main_layout: QHBoxLayout = QHBoxLayout(self.central_widget)
-        self.controls_layout: QVBoxLayout = QVBoxLayout()
-        self.parameters_box: QGroupBox = QGroupBox(self.central_widget)
-        self.parameters_layout: QFormLayout = QFormLayout(self.parameters_box)
-        self.button_topmost: QPushButton = QPushButton(self.central_widget)
-        self.stop_sings_box: QGroupBox = QGroupBox(self.central_widget)
-        self.stop_sings_box.setLayout(QVBoxLayout())
-        self.buttons_layout: QHBoxLayout = QHBoxLayout()
-
-        self.figure: pg.PlotWidget = pg.PlotWidget(self.central_widget)
-
-        self.label_loop_number: pg.ValueLabel = pg.ValueLabel(self.central_widget)
-        self.label_spent_time: pg.ValueLabel = pg.ValueLabel(self.central_widget)
-        self.label_bias: pg.ValueLabel = pg.ValueLabel(self.central_widget)
-        self.label_power: pg.ValueLabel = pg.ValueLabel(self.central_widget)
-        self.label_frequency: pg.ValueLabel = pg.ValueLabel(self.central_widget)
-        self.label_temperature: pg.ValueLabel = pg.ValueLabel(self.central_widget)
-
-        self.stop_key_bias: QPushButton = QPushButton(self.stop_sings_box)
-        self.stop_key_power: QPushButton = QPushButton(self.stop_sings_box)
-        self.stop_key_frequency: QPushButton = QPushButton(self.stop_sings_box)
-        self.stop_key_temperature: QPushButton = QPushButton(self.stop_sings_box)
-
-        self.button_start: QPushButton = QPushButton(self.central_widget)
-        self.button_pause: QPushButton = QPushButton(self.central_widget)
-        self.button_stop: QPushButton = QPushButton(self.central_widget)
-
-        self.setup_ui_appearance()
-        self.load_settings()
-        self.setup_actions()
-
+class GUI(LifetimeGUI):
     def setup_ui_appearance(self) -> None:
-        x_axis: pg.AxisItem = self.figure.getAxis('bottom')
-        x_axis.setLabel(text='Current', units='nA')
-        x_axis.enableAutoSIPrefix(False)
-        y_axis: pg.AxisItem = self.figure.getAxis('left')
-        y_axis.setLabel(text='Lifetime', units='s')
-        y_axis.enableAutoSIPrefix(False)
-        self.figure.plotItem.ctrl.averageGroup.setChecked(False)
-        self.figure.plotItem.ctrl.logYCheck.setChecked(True)
-        self.figure.plotItem.ctrl.xGridCheck.setChecked(True)
-        self.figure.plotItem.ctrl.yGridCheck.setChecked(True)
-        # y_axis.tickStrings = tick_strings
+        super(GUI, self).setup_ui_appearance()
 
-        self.label_spent_time.suffix = 's'
-        self.label_spent_time.formatStr = '{value:0.5f} {suffix}'
-        self.label_loop_number.formatStr = '{value}'
-        self.label_power.suffix = 'dBm'
-        self.label_frequency.suffix = 'GHz'
-        self.label_frequency.formatStr = '{value:0.4f} {suffix}'
-        self.label_bias.suffix = 'nA'
-        self.label_bias.formatStr = '{value:0.2f} {suffix}'
-        self.label_temperature.suffix = 'mK'
-        self.label_temperature.formatStr = '{value:0.2f} {suffix}'
-
-        self.figure.setFocusPolicy(Qt.ClickFocus)
-
-        self.main_layout.addWidget(self.figure)
-        self.main_layout.addLayout(self.controls_layout)
-        self.main_layout.setStretch(0, 1)
-        self.controls_layout.addWidget(self.parameters_box)
-        self.controls_layout.addWidget(self.button_topmost)
-        self.controls_layout.addStretch(1)
-        self.controls_layout.addWidget(self.stop_sings_box)
-        self.controls_layout.addLayout(self.buttons_layout)
-
-        self.parameters_layout.addRow('Since bias set:', self.label_spent_time)
-        self.parameters_layout.addRow('Bias current:', self.label_bias)
-        self.parameters_layout.addRow('Frequency:', self.label_frequency)
-        self.parameters_layout.addRow('Power:', self.label_power)
-        self.parameters_layout.addRow('Temperature:', self.label_temperature)
-        self.parameters_layout.addRow('Loop number:', self.label_loop_number)
-
-        self.button_topmost.setText('Keep the Window Topmost')
-        self.button_topmost.setCheckable(True)
-
-        self.stop_key_bias.setText('Stop after this Bias')
-        self.stop_key_power.setText('Stop after this Power')
-        self.stop_key_frequency.setText('Stop after this Frequency')
-        self.stop_key_temperature.setText('Stop after this Temperature')
-
-        self.stop_key_bias.setCheckable(True)
-        self.stop_key_power.setCheckable(True)
-        self.stop_key_frequency.setCheckable(True)
-        self.stop_key_temperature.setCheckable(True)
-
-        self.stop_sings_box.layout().addWidget(self.stop_key_bias)
-        self.stop_sings_box.layout().addWidget(self.stop_key_power)
-        self.stop_sings_box.layout().addWidget(self.stop_key_frequency)
-        self.stop_sings_box.layout().addWidget(self.stop_key_temperature)
-
-        self.buttons_layout.addWidget(self.button_start)
-        self.buttons_layout.addWidget(self.button_pause)
-        self.buttons_layout.addWidget(self.button_stop)
-
-        self.button_start.setText('Start')
-        self.button_pause.setText('Pause')
-        self.button_pause.setCheckable(True)
-        self.button_stop.setText('Stop')
-        self.button_stop.setDisabled(True)
-
-        self.setCentralWidget(self.central_widget)
-
-    def setup_actions(self):
-        self.button_topmost.toggled.connect(self.on_button_topmost_toggled)
-        self.button_start.clicked.connect(self.on_button_start_clicked)
-        self.button_stop.clicked.connect(self.on_button_stop_clicked)
-
-    def load_settings(self) -> None:
-        self.restoreGeometry(self.settings.value('windowGeometry', b''))
-        self.restoreState(self.settings.value('windowState', b''))
-
-    def save_settings(self) -> None:
-        self.settings.setValue('windowGeometry', self.saveGeometry())
-        self.settings.setValue('windowState', self.saveState())
-        self.settings.sync()
-
-    def closeEvent(self, event: QCloseEvent) -> None:
-        self.on_button_stop_clicked()
-        self.save_settings()
-        event.accept()
-
-    def on_button_topmost_toggled(self, on: bool) -> None:
-        if on:
-            self.setWindowFlags(self.windowFlags() | Qt.CustomizeWindowHint | Qt.WindowStaysOnTopHint)
-            self.show()
-        else:
-            self.setWindowFlags(self.windowFlags() ^ (Qt.CustomizeWindowHint | Qt.WindowStaysOnTopHint))
-            self.show()
-
-    def on_button_start_clicked(self) -> None:
-        self.button_start.setDisabled(True)
-        self.button_pause.setChecked(False)
-        self.button_stop.setEnabled(True)
-
-    def on_button_stop_clicked(self) -> None:
-        self.button_stop.setDisabled(True)
-        self.button_start.setEnabled(True)
+        self.figure.getAxis('bottom').setLabel(text='Current', units='nA')
 
 
 class App(GUI):
---
+++ ui/scd_gui.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+from typing import Dict
+
+import pyqtgraph as pg
+from PyQt5.QtCore import QSettings, Qt
+from PyQt5.QtGui import QCloseEvent, QIcon
+from PyQt5.QtWidgets import (QFormLayout, QGroupBox, QHBoxLayout, QLabel, QMainWindow, QPushButton, QVBoxLayout,
+                             QWidget)
+
+__all__ = ['SwitchingCurrentDistributionGUI']
+
+
+class SwitchingCurrentDistributionGUI(QMainWindow):
+    def __init__(self, flags=Qt.WindowFlags()) -> None:
+        super().__init__(flags=flags)
+
+        self.settings: QSettings = QSettings("SavSoft", "Switching Current Distribution", self)
+
+        self.setWindowTitle('Switching Current Distribution')
+        self.setWindowIcon(QIcon('lsn.svg'))
+
+        self.central_widget: QWidget = QWidget(self)
+        self.main_layout: QHBoxLayout = QHBoxLayout(self.central_widget)
+        self.controls_layout: QVBoxLayout = QVBoxLayout()
+        self.parameters_box: QGroupBox = QGroupBox(self.central_widget)
+        self.parameters_layout: QFormLayout = QFormLayout(self.parameters_box)
+        self.button_topmost: QPushButton = QPushButton(self.central_widget)
+        self.stop_sings_box: QGroupBox = QGroupBox(self.central_widget)
+        self.stop_sings_box.setLayout(QVBoxLayout())
+        self.buttons_layout: QHBoxLayout = QHBoxLayout()
+
+        self.figure: pg.GraphicsLayoutWidget = pg.GraphicsLayoutWidget(self.central_widget)
+        self.canvas_mean: pg.PlotItem = self.figure.addPlot(row=0, col=0)
+        self.canvas_std: pg.PlotItem = self.figure.addPlot(row=1, col=0)
+        self.plot_lines_mean: Dict[int, pg.PlotDataItem] = dict()
+        self.plot_lines_std: Dict[int, pg.PlotDataItem] = dict()
+
+        self.label_loop_number: pg.ValueLabel = pg.ValueLabel(self.central_widget)
+        self.label_remaining_time: QLabel = QLabel(self.central_widget)
+        self.label_mean_current: pg.ValueLabel = pg.ValueLabel(self.central_widget)
+        self.label_std_current: pg.ValueLabel = pg.ValueLabel(self.central_widget)
+        self.label_power: pg.ValueLabel = pg.ValueLabel(self.central_widget)
+        self.label_frequency: pg.ValueLabel = pg.ValueLabel(self.central_widget)
+        self.label_temperature: pg.ValueLabel = pg.ValueLabel(self.central_widget)
+
+        self.stop_key_power: QPushButton = QPushButton(self.stop_sings_box)
+        self.stop_key_frequency: QPushButton = QPushButton(self.stop_sings_box)
+        self.stop_key_temperature: QPushButton = QPushButton(self.stop_sings_box)
+
+        self.button_start: QPushButton = QPushButton(self.central_widget)
+        self.button_pause: QPushButton = QPushButton(self.central_widget)
+        self.button_stop: QPushButton = QPushButton(self.central_widget)
+
+        self.setup_ui_appearance()
+        self.load_settings()
+        self.setup_actions()
+
+    def setup_ui_appearance(self) -> None:
+        x_axis: pg.AxisItem
+        y_axis: pg.AxisItem
+
+        x_axis = self.canvas_mean.getAxis('bottom')
+        x_axis.enableAutoSIPrefix(False)
+        y_axis = self.canvas_mean.getAxis('left')
+        y_axis.setLabel(text='Mean', units='nA')
+        y_axis.enableAutoSIPrefix(False)
+        self.canvas_mean.ctrl.averageGroup.setChecked(False)
+        self.canvas_mean.ctrl.xGridCheck.setChecked(True)
+        self.canvas_mean.ctrl.yGridCheck.setChecked(True)
+
+        x_axis = self.canvas_std.getAxis('bottom')
+        x_axis.enableAutoSIPrefix(False)
+        y_axis = self.canvas_std.getAxis('left')
+        y_axis.setLabel(text='StD', units='nA')
+        y_axis.enableAutoSIPrefix(False)
+        self.canvas_std.ctrl.averageGroup.setChecked(False)
+        self.canvas_std.ctrl.xGridCheck.setChecked(True)
+        self.canvas_std.ctrl.yGridCheck.setChecked(True)
+        self.canvas_std.setXLink(self.canvas_mean)
+
+        self.canvas_mean.addLegend()
+        self.canvas_std.addLegend()
+
+        self.label_loop_number.formatStr = '{value}'
+        self.label_mean_current.suffix = 'nA'
+        self.label_mean_current.formatStr = '{value:0.2f} {suffix}'
+        self.label_std_current.suffix = 'nA'
+        self.label_std_current.formatStr = '{value:0.2f} {suffix}'
+        self.label_power.suffix = 'dBm'
+        self.label_frequency.suffix = 'GHz'
+        self.label_frequency.formatStr = '{value:0.4f} {suffix}'
+        self.label_temperature.suffix = 'mK'
+        self.label_temperature.formatStr = '{value:0.2f} {suffix}'
+
+        self.figure.setFocusPolicy(Qt.ClickFocus)
+
+        self.main_layout.addWidget(self.figure)
+        self.main_layout.addLayout(self.controls_layout)
+        self.main_layout.setStretch(0, 1)
+        self.controls_layout.addWidget(self.parameters_box)
+        self.controls_layout.addWidget(self.button_topmost)
+        self.controls_layout.addStretch(1)
+        self.controls_layout.addWidget(self.stop_sings_box)
+        self.controls_layout.addLayout(self.buttons_layout)
+
+        self.parameters_layout.addRow('Remaining time:', self.label_remaining_time)
+        self.parameters_layout.addRow('Mean current:', self.label_mean_current)
+        self.parameters_layout.addRow('Current std:', self.label_std_current)
+        self.parameters_layout.addRow('Frequency:', self.label_frequency)
+        self.parameters_layout.addRow('Power:', self.label_power)
+        self.parameters_layout.addRow('Temperature:', self.label_temperature)
+        self.parameters_layout.addRow('Loop number:', self.label_loop_number)
+
+        self.button_topmost.setText('Keep the Window Topmost')
+        self.button_topmost.setCheckable(True)
+
+        self.stop_key_power.setText('Stop after this Power')
+        self.stop_key_frequency.setText('Stop after this Frequency')
+        self.stop_key_temperature.setText('Stop after this Temperature')
+
+        self.stop_key_power.setCheckable(True)
+        self.stop_key_frequency.setCheckable(True)
+        self.stop_key_temperature.setCheckable(True)
+
+        self.stop_sings_box.layout().addWidget(self.stop_key_power)
+        self.stop_sings_box.layout().addWidget(self.stop_key_frequency)
+        self.stop_sings_box.layout().addWidget(self.stop_key_temperature)
+
+        self.buttons_layout.addWidget(self.button_start)
+        self.buttons_layout.addWidget(self.button_pause)
+        self.buttons_layout.addWidget(self.button_stop)
+
+        self.button_start.setText('Start')
+        self.button_pause.setText('Pause')
+        self.button_pause.setCheckable(True)
+        self.button_stop.setText('Stop')
+        self.button_stop.setDisabled(True)
+
+        self.setCentralWidget(self.central_widget)
+
+    def setup_actions(self):
+        self.button_topmost.toggled.connect(self.on_button_topmost_toggled)
+        self.button_start.clicked.connect(self.on_button_start_clicked)
+        self.button_stop.clicked.connect(self.on_button_stop_clicked)
+
+    def load_settings(self) -> None:
+        self.restoreGeometry(self.settings.value('windowGeometry', b''))
+        self.restoreState(self.settings.value('windowState', b''))
+
+    def save_settings(self) -> None:
+        self.settings.setValue('windowGeometry', self.saveGeometry())
+        self.settings.setValue('windowState', self.saveState())
+        self.settings.sync()
+
+    def closeEvent(self, event: QCloseEvent) -> None:
+        self.on_button_stop_clicked()
+        self.save_settings()
+        event.accept()
+
+    def on_button_topmost_toggled(self, on: bool) -> None:
+        if on:
+            self.setWindowFlags(self.windowFlags() | Qt.CustomizeWindowHint | Qt.WindowStaysOnTopHint)
+            self.show()
+        else:
+            self.setWindowFlags(self.windowFlags() ^ (Qt.CustomizeWindowHint | Qt.WindowStaysOnTopHint))
+            self.show()
+
+    def on_button_start_clicked(self) -> None:
+        self.button_start.setDisabled(True)
+        self.button_pause.setChecked(False)
+        self.button_stop.setEnabled(True)
+
+    def on_button_stop_clicked(self) -> None:
+        self.button_stop.setDisabled(True)
+        self.button_start.setEnabled(True)

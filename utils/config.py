b3ffd8d7a7a43bef1cae0f1955d70f850d6b8df6
StSav012 <stsav012@gmail.com>
2024-01-29 20:52:14 +0300

    Provide a class dedicated to the measurements

--- utils/config.py
+++ utils/config.py
@@ -3,12 +3,14 @@ from __future__ import annotations
 
 from configparser import ConfigParser
 from decimal import Decimal
-from typing import Any, LiteralString, Sequence
+from os import PathLike
+from typing import Any, Iterable, LiteralString, Sequence, overload
 
 from .si import parse_si_number
 from .slice_sequence import SliceSequence
 
 __all__ = [
+    "Config",
     "get_str",
     "get_bool",
     "get_int",
@@ -228,4 +230,182 @@ def get_slice_sequence(
             config.get(f"{section}", key),
             slice_separator=slice_separator,
             items_separator=items_separator,
+        )
+
+
+class Config(ConfigParser):
+    def __init__(
+        self,
+        filenames: (
+            str | bytes | PathLike[str] | PathLike[bytes] | Iterable[str | bytes | PathLike[str] | PathLike[bytes]]
+        ) = "config.ini",
+    ) -> None:
+        super().__init__(
+            allow_no_value=True,
+            inline_comment_prefixes=("#", ";"),
+        )
+
+        self.read(filenames=filenames)
+
+        self._sample_name: str = self.get(section="circuitry", option="sample name")
+
+    @property
+    def sample_name(self) -> str:
+        return self._sample_name
+
+    @overload
+    def get_str(self, section: LiteralString, key: LiteralString) -> str:
+        ...
+
+    @overload
+    def get_str(self, section: LiteralString, key: LiteralString, *, fallback: str) -> str:
+        ...
+
+    def get_str(self, section: LiteralString, key: LiteralString, **kwargs: str) -> str:
+        return get_str(self, self._sample_name, section, key, **kwargs)
+
+    @overload
+    def get_bool(self, section: LiteralString, key: LiteralString) -> bool:
+        ...
+
+    @overload
+    def get_bool(self, section: LiteralString, key: LiteralString, *, fallback: bool) -> bool:
+        ...
+
+    def get_bool(self, section: LiteralString, key: LiteralString, **kwargs: bool) -> bool:
+        return get_bool(self, self._sample_name, section, key, **kwargs)
+
+    @overload
+    def get_int(self, section: LiteralString, key: LiteralString) -> int:
+        ...
+
+    @overload
+    def get_int(self, section: LiteralString, key: LiteralString, *, fallback: int) -> int:
+        ...
+
+    def get_int(self, section: LiteralString, key: LiteralString, **kwargs) -> int:
+        return get_int(self, self._sample_name, section, key, **kwargs)
+
+    @overload
+    def get_float(self, section: LiteralString, key: LiteralString) -> float:
+        ...
+
+    @overload
+    def get_float(self, section: LiteralString, key: LiteralString, *, fallback: float) -> float:
+        ...
+
+    def get_float(self, section: LiteralString, key: LiteralString, **kwargs: float) -> float:
+        return get_float(self, self._sample_name, section, key, **kwargs)
+
+    @overload
+    def get_decimal(self, section: LiteralString, key: LiteralString) -> Decimal:
+        ...
+
+    @overload
+    def get_decimal(self, section: LiteralString, key: LiteralString, *, fallback: float) -> Decimal:
+        ...
+
+    def get_decimal(self, section: LiteralString, key: LiteralString, **kwargs: float) -> Decimal:
+        return get_decimal(self, self._sample_name, section, key, **kwargs)
+
+    @overload
+    def get_float_tuple(self, section: LiteralString, key: LiteralString, *, separator: str = ",") -> tuple[float, ...]:
+        ...
+
+    @overload
+    def get_float_tuple(
+        self, section: LiteralString, key: LiteralString, *, fallback: Sequence[float], separator: str = ","
+    ) -> tuple[float, ...]:
+        ...
+
+    def get_float_tuple(
+        self,
+        section: LiteralString,
+        key: LiteralString,
+        *,
+        separator: str = ",",
+        **kwargs: Sequence[float],
+    ) -> tuple[float, ...]:
+        return get_float_tuple(self, self._sample_name, section, key, separator=separator, **kwargs)
+
+    @overload
+    def get_float_list(self, section: LiteralString, key: LiteralString, *, separator: str = ",") -> list[float]:
+        ...
+
+    @overload
+    def get_float_list(
+        self, section: LiteralString, key: LiteralString, *, fallback: Sequence[float], separator: str = ","
+    ) -> list[float]:
+        ...
+
+    def get_float_list(
+        self,
+        section: LiteralString,
+        key: LiteralString,
+        *,
+        separator: str = ",",
+        **kwargs: Sequence[float],
+    ) -> list[float]:
+        return get_float_list(self, self._sample_name, section, key, separator=separator, **kwargs)
+
+    @overload
+    def get_decimal_list(self, section: LiteralString, key: LiteralString, *, separator: str = ",") -> list[Decimal]:
+        ...
+
+    @overload
+    def get_decimal_list(
+        self, section: LiteralString, key: LiteralString, *, fallback: Sequence[float], separator: str = ","
+    ) -> list[Decimal]:
+        ...
+
+    def get_decimal_list(
+        self,
+        section: LiteralString,
+        key: LiteralString,
+        *,
+        separator: str = ",",
+        **kwargs: Sequence[float],
+    ) -> list[Decimal]:
+        return get_decimal_list(self, self._sample_name, section, key, separator=separator, **kwargs)
+
+    @overload
+    def get_slice_sequence(
+        self,
+        section: LiteralString,
+        key: LiteralString,
+        *,
+        slice_separator: str | Sequence[str] = ("..", ":"),
+        items_separator: str | Sequence[str] = (",", ";"),
+    ) -> SliceSequence:
+        ...
+
+    @overload
+    def get_slice_sequence(
+        self,
+        section: LiteralString,
+        key: LiteralString,
+        *,
+        fallback: SliceSequence,
+        slice_separator: str | Sequence[str] = ("..", ":"),
+        items_separator: str | Sequence[str] = (",", ";"),
+    ) -> SliceSequence:
+        ...
+
+    def get_slice_sequence(
+        self,
+        section: LiteralString,
+        key: LiteralString,
+        *,
+        slice_separator: str | Sequence[str] = ("..", ":"),
+        items_separator: str | Sequence[str] = (",", ";"),
+        **kwargs: SliceSequence,
+    ) -> SliceSequence:
+        return get_slice_sequence(
+            self,
+            self._sample_name,
+            section,
+            key,
+            slice_separator=slice_separator,
+            items_separator=items_separator,
+            **kwargs,
         )

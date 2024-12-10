5bb08fb5a3bf7bede1f47359fe609ee51d16d25c
StSav012 <stsav012@gmail.com>
2024-12-10 12:50:38 +0300

    Add scaling and window options

--- spectrum gui.py
+++ spectrum gui.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 import sys
 from multiprocessing import Queue
+from queue import Empty
 from typing import Final, final
 
 import numpy as np
@@ -12,6 +13,7 @@ from qtpy.QtCore import QSettings, QTimer, Qt, Slot
 from qtpy.QtGui import QCloseEvent, QIcon
 from qtpy.QtWidgets import (
     QApplication,
+    QCheckBox,
     QDoubleSpinBox,
     QFormLayout,
     QGroupBox,
@@ -21,10 +23,12 @@ from qtpy.QtWidgets import (
     QVBoxLayout,
     QWidget,
 )
+from scipy.signal import welch
 
 from hardware import device_adc
 from measurement.noise import NoiseMeasurement
-from utils.processing import welch
+from utils import clear_queue_after_process, silent_alive
+from utils.processing import get_scipy_signal_windows_by_name
 
 _MAX_ADC_SAMPLE_RATE: Final[float] = device_adc.ai_max_multi_chan_rate
 
@@ -54,6 +58,8 @@ class GUI(QMainWindow):
         self.combo_channel: pg.ComboBox = pg.ComboBox(self.central_widget)
         self.spin_sample_rate: pg.SpinBox = pg.SpinBox(self.central_widget)
         self.spin_scale: pg.SpinBox = pg.SpinBox(self.central_widget)
+        self.check_power_or_magnitude: QCheckBox = QCheckBox(self.central_widget)
+        self.combo_welch_window: pg.ComboBox = pg.ComboBox(self.central_widget)
         self.spin_averaging_time_span: QDoubleSpinBox = QDoubleSpinBox(self.central_widget)
         self.spin_display_time_span: QDoubleSpinBox = QDoubleSpinBox(self.central_widget)
 
@@ -79,6 +85,10 @@ class GUI(QMainWindow):
         }
         self.spin_sample_rate.setOpts(**opts)
         self.spin_sample_rate.setRange(1.0, _MAX_ADC_SAMPLE_RATE)
+
+        self.check_power_or_magnitude.setText(self.tr("Power units"))
+
+        self.combo_welch_window.setItems({key: value for value, key in get_scipy_signal_windows_by_name()})
 
         self.spin_averaging_time_span.setMinimum(2.0 / _MAX_ADC_SAMPLE_RATE)
         self.spin_averaging_time_span.setMaximum(np.inf)
@@ -114,6 +124,8 @@ class GUI(QMainWindow):
 
         self.parameters_layout.addRow(self.tr("Sample rate:"), self.spin_sample_rate)
         self.parameters_layout.addRow(self.tr("Voltage scale:"), self.spin_scale)
+        self.parameters_layout.addWidget(self.check_power_or_magnitude)
+        self.parameters_layout.addRow(self.tr("Window:"), self.combo_welch_window)
         self.parameters_layout.addRow(self.tr("Time to average over:"), self.spin_averaging_time_span)
         self.parameters_layout.addRow(self.tr("Time span to show:"), self.spin_display_time_span)
 
@@ -137,6 +149,8 @@ class GUI(QMainWindow):
         self.settings.beginGroup("parameters")
         self.spin_sample_rate.setValue(self.settings.value("sampleRate", 32678.0, float))
         self.spin_scale.setValue(self.settings.value("voltageScale", 100.0, float))
+        self.check_power_or_magnitude.setChecked(self.settings.value("powerUnits", True, bool))
+        self.combo_welch_window.setValue(self.settings.value("welchWindow", "hann", str))
         self.spin_averaging_time_span.setValue(self.settings.value("averagingTimeSpan", 20.0, float))
         self.spin_display_time_span.setValue(self.settings.value("displayTimeSpan", 2.0, float))
         self.settings.endGroup()
@@ -148,6 +162,8 @@ class GUI(QMainWindow):
         self.settings.beginGroup("parameters")
         self.settings.setValue("sampleRate", self.spin_sample_rate.value())
         self.settings.setValue("voltageScale", self.spin_scale.value())
+        self.settings.setValue("powerUnits", self.check_power_or_magnitude.isChecked())
+        self.settings.setValue("welchWindow", self.combo_welch_window.value())
         self.settings.setValue("averagingTimeSpan", self.spin_averaging_time_span.value())
         self.settings.setValue("displayTimeSpan", self.spin_display_time_span.value())
         self.settings.endGroup()
@@ -184,6 +200,46 @@ class App(GUI):
 
         self.v: NDArray[np.float64] = np.empty(0)
 
+        self.check_power_or_magnitude.toggled.connect(self.on_check_power_or_magnitude_toggled)
+        self.combo_welch_window.currentTextChanged.connect(self.on_combo_welch_window_current_text_changed)
+
+    @Slot(bool)
+    def on_check_power_or_magnitude_toggled(self, checked: bool) -> None:
+        if checked:
+            self.canvas_voltage_spectrum.setLabels(
+                left=(self.tr("Voltage PSD"), self.tr("V² / Hz")),
+                bottom=(self.tr("Frequency"), self.tr("Hz")),
+            )
+        else:
+            self.canvas_voltage_spectrum.setLabels(
+                left=(self.tr("Voltage PSD"), self.tr("V / sqrt(Hz)")),
+                bottom=(self.tr("Frequency"), self.tr("Hz")),
+            )
+
+        if not self.v.size:
+            return
+
+        x_data: NDArray[np.float64] | None = self.voltage_trend_plot_line.xData
+        if x_data is None:
+            return
+        sample_rate: float = (x_data.shape[0] - 1) / (x_data[-1] - x_data[0])
+
+        self._draw_spectrum(sample_rate)
+
+    @Slot(str)
+    def on_combo_welch_window_current_text_changed(self, text: str) -> None:
+        if not text:
+            return
+        if not self.v.size:
+            return
+
+        x_data: NDArray[np.float64] | None = self.voltage_trend_plot_line.xData
+        if x_data is None:
+            return
+        sample_rate: float = (x_data.shape[0] - 1) / (x_data[-1] - x_data[0])
+
+        self._draw_spectrum(sample_rate)
+
     @Slot()
     def on_button_start_clicked(self) -> None:
         super().on_button_start_clicked()
@@ -198,9 +254,12 @@ class App(GUI):
 
     @Slot()
     def on_button_stop_clicked(self) -> None:
-        if self.measurement is not None:
-            self.measurement.terminate()
+        if silent_alive(self.measurement):
+            self.measurement.stop()
             self.measurement.join()
+        else:
+            clear_queue_after_process(self.measurement, self.results_queue)
+            self.measurement = None
         self.timer.stop()
         super().on_button_stop_clicked()
 
@@ -211,18 +270,40 @@ class App(GUI):
         points_to_display: int
         points_for_spectrum: int
         while self.results_queue.qsize():
-            sample_rate, v = self.results_queue.get()
-            points_to_display = round(sample_rate * self.spin_display_time_span.value())
-            points_for_spectrum = round(sample_rate * self.spin_averaging_time_span.value())
-            self.v = np.concatenate((self.v, v))[-max(points_to_display, points_for_spectrum) :]
-        if not np.isnan(sample_rate):
-            points_to_display = round(sample_rate * self.spin_display_time_span.value())
-            points_for_spectrum = round(sample_rate * self.spin_averaging_time_span.value())
-            self.voltage_trend_plot_line.setData(
-                np.arange(min(self.v.size, points_to_display)) / sample_rate,
-                self.v[-points_to_display:],
-            )
-            self.voltage_spectrum_plot_line.setData(*welch(self.v[-points_for_spectrum:], sample_rate=sample_rate))
+            try:
+                sample_rate, v = self.results_queue.get()
+            except Empty:
+                return
+            else:
+                points_to_display = round(sample_rate * self.spin_display_time_span.value())
+                points_for_spectrum = round(sample_rate * self.spin_averaging_time_span.value())
+                self.v = np.concatenate((self.v, v))[-max(points_to_display, points_for_spectrum) :]
+
+        points_to_display = round(sample_rate * self.spin_display_time_span.value())
+        self.voltage_trend_plot_line.setData(
+            np.arange(min(self.v.size, points_to_display)) / sample_rate,
+            self.v[-points_to_display:],
+        )
+
+        self._draw_spectrum(sample_rate)
+
+    def _draw_spectrum(self, sample_rate: float) -> None:
+        points_to_display: int = round(sample_rate * self.spin_display_time_span.value())
+        points_for_spectrum: int = round(sample_rate * self.spin_averaging_time_span.value())
+        if points_for_spectrum <= 0:
+            return
+        freq: NDArray[np.float64]
+        pn_xx: NDArray[np.float64]
+        freq, pn_xx = welch(
+            self.v[-points_for_spectrum:],
+            fs=sample_rate,
+            nperseg=points_to_display,
+            window=self.combo_welch_window.value(),
+        )
+        if self.check_power_or_magnitude.isChecked():
+            self.voltage_spectrum_plot_line.setData(freq, pn_xx)
+        else:
+            self.voltage_spectrum_plot_line.setData(freq, np.sqrt(pn_xx))
 
 
 if __name__ == "__main__":

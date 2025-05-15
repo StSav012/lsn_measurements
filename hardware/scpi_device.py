286502749c24d342482703324ee2b0d45ba98bad
StSav012 <stsav012@gmail.com>
2025-05-15 15:54:56 +0300

    Add functions for faster commands' description

--- hardware/scpi_device.py
+++ hardware/scpi_device.py
@@ -1,9 +1,13 @@
+from collections.abc import Callable, Collection
+from math import nan
 from socket import AF_INET, SOCK_STREAM, socket
+from typing import Any, ClassVar, Final
 
 from utils import warning
 from utils.port_scanner import port_scanner
+from utils.string_utils import find_single_matching_string, to_bool
 
-__all__ = ["SCPIDevice"]
+__all__ = ["SCPIDevice", "SCPIDeviceSubCategory"]
 
 
 class SCPIDevice:
@@ -50,16 +54,49 @@ class SCPIDevice:
                 if reset:
                     self.reset()
 
+    @staticmethod
+    def property_by_command[T](
+        cmd: str,
+        values: type[T] | Collection[str],
+        *,
+        parameter: T | None = None,
+        read_only: bool = False,
+        doc: str = "",
+    ) -> property:
+        def getter(self: "SCPIDevice") -> T:
+            if self.socket is None:
+                if values is float:
+                    return nan
+                return values()
+            if values is bool:
+                return to_bool(self.query(cmd, parameter=parameter))
+            if values is int:
+                return int(float(self.query(cmd, parameter=parameter)))
+            if not isinstance(values, str) and isinstance(values, Collection):
+                return find_single_matching_string(self.query(cmd, parameter=parameter), values)
+            return values(self.query(cmd, parameter=parameter))
+
+        setter: Callable[[Any, Any], None] | None
+        if read_only:
+            setter = None
+        else:
+
+            def setter(self: "SCPIDevice", new_value: T) -> None:
+                if self.socket is None:
+                    return
+                if not isinstance(values, str) and isinstance(values, Collection):
+                    self.issue(cmd, find_single_matching_string(new_value, values))
+                else:
+                    self.issue(cmd, values(new_value))
+
+        return property(getter, setter, None, doc or f"Query and set {cmd}")
+
     def __del__(self) -> None:
         if self.socket is not None:
             self.socket.close()
 
-    @property
-    def idn(self) -> str:
-        return self.communicate("*idn?")
-
-    def opc(self) -> bool:
-        return bool(self.communicate("*opc?"))
+    idn: property = property_by_command("*idn?", str, read_only=True)
+    opc: property = property_by_command("*opc?", bool, read_only=True)
 
     def reset(self) -> None:
         self.communicate("*rst")
@@ -75,7 +112,7 @@ class SCPIDevice:
             resp += self.socket.recv(1)
             if not resp:
                 return ""
-        return resp.decode().strip()
+        return resp.decode("ascii").strip()
 
     def query(self, command: str, parameter: object | None = None) -> str:
         command = command.strip()
@@ -91,3 +128,52 @@ class SCPIDevice:
         if isinstance(value, bool):
             value = {False: "OFF", True: "ON"}[value]
         self.communicate(command.rstrip("?") + " " + str(value).rstrip("?"))
+
+
+class SCPIDeviceSubCategory:
+    prefix: ClassVar[str] = ...
+
+    def __init__(self, parent: SCPIDevice) -> None:
+        if self.__class__.prefix is ...:
+            raise ValueError("Specify the sub-category prefix")
+
+        self.parent: Final[SCPIDevice] = parent
+
+    @staticmethod
+    def subproperty_by_command[T](
+        cmd: str,
+        values: type[T] | Collection[str],
+        *,
+        parameter: T | None = None,
+        read_only: bool = False,
+        doc: str = "",
+    ) -> property:
+        def getter(self: SCPIDeviceSubCategory) -> T:
+            if self.parent.socket is None:
+                if values is float:
+                    return nan
+                return values()
+            subcmd: str = ":".join((self.__class__.prefix, cmd)) if cmd else self.__class__.prefix
+            if values is bool:
+                return to_bool(self.parent.query(subcmd, parameter=parameter))
+            if values is int:
+                return int(float(self.parent.query(subcmd, parameter=parameter)))
+            if not isinstance(values, str) and isinstance(values, Collection):
+                return find_single_matching_string(self.parent.query(subcmd, parameter=parameter), values)
+            return values(self.parent.query(subcmd, parameter=parameter))
+
+        setter: Callable[[Any, Any], None] | None
+        if read_only:
+            setter = None
+        else:
+
+            def setter(self: SCPIDeviceSubCategory, new_value: T) -> None:
+                if self.parent.socket is None:
+                    return
+                subcmd: str = ":".join((self.__class__.prefix, cmd)) if cmd else self.__class__.prefix
+                if not isinstance(values, str) and isinstance(values, Collection):
+                    self.parent.issue(subcmd, find_single_matching_string(new_value, values))
+                else:
+                    self.parent.issue(subcmd, values(new_value))
+
+        return property(getter, setter, None, doc or f"Query and set {cmd}")

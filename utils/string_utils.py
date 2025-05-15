860571d35c4db1b32ed6f23ffbff58b617766eff
StSav012 <stsav012@gmail.com>
2025-05-15 12:15:01 +0300

    Add `find_single_matching_string`

--- utils/string_utils.py
+++ utils/string_utils.py
@@ -1,5 +1,6 @@
-from collections.abc import Sequence
+from collections.abc import Collection, Sequence
 from functools import lru_cache
+from itertools import takewhile
 
 __all__ = [
     "decimals",
@@ -7,6 +8,7 @@ __all__ = [
     "multi_split",
     "nth_occurrence",
     "seconds_to_time",
+    "find_single_matching_string",
 ]
 
 
@@ -56,3 +58,22 @@ def nth_occurrence(text: str, substrings: str | Sequence[str], n: int) -> int:
             if count == n:
                 return index
     return -1
+
+
+def find_single_matching_string(s: str, variants: Collection[str]) -> str:
+    if s in variants:
+        return s
+
+    numeric_suffix: str = "".join(takewhile(str.isnumeric, s[::-1]))[::-1]
+    s = s.casefold()
+    matching_variants: list[str]
+    if numeric_suffix:
+        s = s[: -len(numeric_suffix)]
+        matching_variants = [v for v in variants if v.casefold().startswith(s) and v.endswith(numeric_suffix)]
+    else:
+        matching_variants = [v for v in variants if v.casefold().startswith(s)]
+    if len(matching_variants) == 1:
+        return matching_variants[0]
+    if matching_variants:
+        raise ValueError(f"Ambiguous value: {s!r}. Possible variants: {matching_variants}")
+    raise ValueError(f"Ambiguous value: {s!r}. Provided variants: {variants}")

bf4fbf4e76f65736c580dd8ff1bb788e858e030b
StSav012 <stsav012@gmail.com>
2025-08-28 21:59:06 +0300

    Refactor to shorten the code

--- utils/config.py
+++ utils/config.py
@@ -7,228 +7,12 @@ from typing import LiteralString, overload
 from .si import parse_si_number
 from .slice_sequence import SliceSequence
 
-__all__ = [
-    "Config",
-    "get_bool",
-    "get_decimal",
-    "get_decimal_list",
-    "get_float",
-    "get_float_list",
-    "get_float_tuple",
-    "get_int",
-    "get_str",
-]
-
-__sentinel = object()
-
-
-def get(
-    config: ConfigParser,
-    sample: str,
-    section: LiteralString,
-    key: LiteralString,
-    *,
-    fallback: str = __sentinel,
-) -> str:
-    if f"{section}/{sample}" in config.sections() and key in config[f"{section}/{sample}"]:
-        if fallback is not __sentinel:
-            return config.get(f"{section}/{sample}", key, fallback=fallback)
-        return config.get(f"{section}/{sample}", key)
-    if fallback is not __sentinel:
-        return config.get(f"{section}", key, fallback=fallback)
-    return config.get(f"{section}", key)
-
-
-def get_str(
-    config: ConfigParser,
-    sample: str,
-    section: LiteralString,
-    key: LiteralString,
-    *,
-    fallback: str = __sentinel,
-) -> str:
-    return get(config=config, sample=sample, section=section, key=key, fallback=fallback)
-
-
-def get_bool(
-    config: ConfigParser,
-    sample: str,
-    section: LiteralString,
-    key: LiteralString,
-    *,
-    fallback: bool = __sentinel,
-) -> bool:
-    if f"{section}/{sample}" in config.sections() and key in config[f"{section}/{sample}"]:
-        if fallback is not __sentinel:
-            return config.getboolean(f"{section}/{sample}", key, fallback=fallback)
-        return config.getboolean(f"{section}/{sample}", key)
-    if fallback is not __sentinel:
-        return config.getboolean(f"{section}", key, fallback=fallback)
-    return config.getboolean(f"{section}", key)
-
-
-def get_int(
-    config: ConfigParser,
-    sample: str,
-    section: LiteralString,
-    key: LiteralString,
-    *,
-    fallback: int = __sentinel,
-) -> int:
-    if f"{section}/{sample}" in config.sections() and key in config[f"{section}/{sample}"]:
-        if fallback is not __sentinel:
-            return config.getint(f"{section}/{sample}", key, fallback=fallback)
-        return config.getint(f"{section}/{sample}", key)
-    if fallback is not __sentinel:
-        return config.getint(f"{section}", key, fallback=fallback)
-    return config.getint(f"{section}", key)
-
-
-def get_float(
-    config: ConfigParser,
-    sample: str,
-    section: LiteralString,
-    key: LiteralString,
-    *,
-    fallback: float = __sentinel,
-) -> float:
-    if f"{section}/{sample}" in config.sections() and key in config[f"{section}/{sample}"]:
-        if fallback is not __sentinel:
-            return parse_si_number(config.get(f"{section}/{sample}", key, fallback=fallback))
-        return parse_si_number(config.get(f"{section}/{sample}", key))
-    if fallback is not __sentinel:
-        return parse_si_number(config.get(f"{section}", key, fallback=fallback))
-    return parse_si_number(config.get(f"{section}", key))
-
-
-def get_decimal(
-    config: ConfigParser,
-    sample: str,
-    section: LiteralString,
-    key: LiteralString,
-    *,
-    fallback: Decimal | float | str | tuple[int, Sequence[int], int] = __sentinel,
-) -> Decimal:
-    return Decimal(get(config=config, sample=sample, section=section, key=key, fallback=fallback))
-
-
-def get_float_tuple(
-    config: ConfigParser,
-    sample: str,
-    section: LiteralString,
-    key: LiteralString,
-    *,
-    fallback: Sequence[float] = __sentinel,
-    separator: str = ",",
-) -> tuple[float, ...]:
-    try:
-        return tuple(
-            map(
-                float,
-                get(
-                    config=config,
-                    sample=sample,
-                    section=section,
-                    key=key,
-                ).split(separator),
-            ),
-        )
-    except LookupError:
-        if fallback is not __sentinel:
-            return tuple(fallback)
-        raise
-
-
-def get_float_list(
-    config: ConfigParser,
-    sample: str,
-    section: LiteralString,
-    key: LiteralString,
-    *,
-    fallback: Sequence[float] = __sentinel,
-    separator: str = ",",
-) -> list[float]:
-    try:
-        return list(
-            map(
-                float,
-                get(
-                    config=config,
-                    sample=sample,
-                    section=section,
-                    key=key,
-                ).split(separator),
-            ),
-        )
-    except LookupError:
-        if fallback is not __sentinel:
-            return list(fallback)
-        raise
-
-
-def get_decimal_list(
-    config: ConfigParser,
-    sample: str,
-    section: LiteralString,
-    key: LiteralString,
-    *,
-    fallback: Sequence[float] = __sentinel,
-    separator: str = ",",
-) -> list[Decimal]:
-    try:
-        return list(
-            map(
-                Decimal,
-                get(
-                    config=config,
-                    sample=sample,
-                    section=section,
-                    key=key,
-                ).split(separator),
-            ),
-        )
-    except LookupError:
-        if fallback is not __sentinel:
-            return list(map(Decimal, fallback))
-        raise
-
-
-def get_slice_sequence(
-    config: ConfigParser,
-    sample: str,
-    section: LiteralString,
-    key: LiteralString,
-    *,
-    fallback: SliceSequence = __sentinel,
-    slice_separator: str | Sequence[str] = ("..", ":"),
-    items_separator: str | Sequence[str] = (",", ";"),
-) -> SliceSequence:
-    if f"{section}/{sample}" in config.sections() and key in config[f"{section}/{sample}"]:
-        if fallback is not __sentinel:
-            return SliceSequence(
-                config.get(f"{section}/{sample}", key, fallback=fallback),
-                slice_separator=slice_separator,
-                items_separator=items_separator,
-            )
-        return SliceSequence(
-            config.get(f"{section}/{sample}", key),
-            slice_separator=slice_separator,
-            items_separator=items_separator,
-        )
-    if fallback is not __sentinel:
-        return SliceSequence(
-            config.get(f"{section}", key, fallback=fallback),
-            slice_separator=slice_separator,
-            items_separator=items_separator,
-        )
-    return SliceSequence(
-        config.get(f"{section}", key),
-        slice_separator=slice_separator,
-        items_separator=items_separator,
-    )
+__all__ = ["Config"]
 
 
 class Config(ConfigParser):
+    __sentinel = object()
+
     def __init__(
         self,
         filenames: (
@@ -242,11 +26,36 @@ class Config(ConfigParser):
 
         self.read(filenames=filenames)
 
-        self._sample_name: str = self.get(section="circuitry", option="sample name")
+        self._sample_name: str = super().get(section="circuitry", option="sample name")
 
     @property
     def sample_name(self) -> str:
         return self._sample_name
+
+    def _full_section(self, section: str, key: str) -> str:
+        sample: str = self._sample_name
+        if f"{section}/{sample}" in self.sections() and key in self[f"{section}/{sample}"]:
+            return f"{section}/{sample}"
+        return section
+
+    # noinspection PyMethodOverriding
+    @overload
+    def get(self, section: str, key: str) -> str: ...
+    # noinspection PyMethodOverriding
+    @overload
+    def get(self, section: str, key: str, *, fallback: object) -> str: ...
+    # noinspection PyMethodOverriding
+    def get(
+        self,
+        section: LiteralString,
+        key: LiteralString,
+        *,
+        fallback: object = __sentinel,
+    ) -> str:
+        full_section: str = self._full_section(section=section, key=key)
+        if fallback is not Config.__sentinel:
+            return super().get(full_section, key, fallback=fallback)
+        return super().get(full_section, key)
 
     @overload
     def get_str(self, section: LiteralString, key: LiteralString) -> str: ...
@@ -254,8 +63,8 @@ class Config(ConfigParser):
     @overload
     def get_str(self, section: LiteralString, key: LiteralString, *, fallback: str) -> str: ...
 
-    def get_str(self, section: LiteralString, key: LiteralString, **kwargs: str) -> str:
-        return get_str(self, self._sample_name, section, key, **kwargs)
+    def get_str(self, section: LiteralString, key: LiteralString, *, fallback: str = __sentinel) -> str:
+        return self.get(section=section, key=key, fallback=fallback)
 
     @overload
     def get_bool(self, section: LiteralString, key: LiteralString) -> bool: ...
@@ -263,8 +72,11 @@ class Config(ConfigParser):
     @overload
     def get_bool(self, section: LiteralString, key: LiteralString, *, fallback: bool) -> bool: ...
 
-    def get_bool(self, section: LiteralString, key: LiteralString, **kwargs: bool) -> bool:
-        return get_bool(self, self._sample_name, section, key, **kwargs)
+    def get_bool(self, section: LiteralString, key: LiteralString, *, fallback: bool = __sentinel) -> bool:
+        full_section: str = self._full_section(section=section, key=key)
+        if fallback is not Config.__sentinel:
+            return self.getboolean(full_section, key, fallback=fallback)
+        return self.getboolean(full_section, key)
 
     @overload
     def get_int(self, section: LiteralString, key: LiteralString) -> int: ...
@@ -272,8 +84,8 @@ class Config(ConfigParser):
     @overload
     def get_int(self, section: LiteralString, key: LiteralString, *, fallback: int) -> int: ...
 
-    def get_int(self, section: LiteralString, key: LiteralString, **kwargs) -> int:
-        return get_int(self, self._sample_name, section, key, **kwargs)
+    def get_int(self, section: LiteralString, key: LiteralString, *, fallback: int = __sentinel) -> int:
+        return int(self.get(section=section, key=key, fallback=fallback))
 
     @overload
     def get_float(self, section: LiteralString, key: LiteralString) -> float: ...
@@ -281,8 +93,8 @@ class Config(ConfigParser):
     @overload
     def get_float(self, section: LiteralString, key: LiteralString, *, fallback: float) -> float: ...
 
-    def get_float(self, section: LiteralString, key: LiteralString, **kwargs: float) -> float:
-        return get_float(self, self._sample_name, section, key, **kwargs)
+    def get_float(self, section: LiteralString, key: LiteralString, *, fallback: float = __sentinel) -> float:
+        return parse_si_number(self.get(section=section, key=key, fallback=fallback))
 
     @overload
     def get_decimal(self, section: LiteralString, key: LiteralString) -> Decimal: ...
@@ -290,8 +102,8 @@ class Config(ConfigParser):
     @overload
     def get_decimal(self, section: LiteralString, key: LiteralString, *, fallback: float) -> Decimal: ...
 
-    def get_decimal(self, section: LiteralString, key: LiteralString, **kwargs: float) -> Decimal:
-        return get_decimal(self, self._sample_name, section, key, **kwargs)
+    def get_decimal(self, section: LiteralString, key: LiteralString, *, fallback: float = __sentinel) -> Decimal:
+        return Decimal(self.get(section=section, key=key, fallback=fallback))
 
     @overload
     def get_float_tuple(
@@ -318,9 +130,19 @@ class Config(ConfigParser):
         key: LiteralString,
         *,
         separator: str = ",",
-        **kwargs: Sequence[float],
+        fallback: Sequence[float] = __sentinel,
     ) -> tuple[float, ...]:
-        return get_float_tuple(self, self._sample_name, section, key, separator=separator, **kwargs)
+        try:
+            return tuple(
+                map(
+                    float,
+                    self.get(section=section, key=key, fallback=fallback).split(separator),
+                ),
+            )
+        except LookupError:
+            if fallback is not Config.__sentinel:
+                return tuple(fallback)
+            raise
 
     @overload
     def get_float_list(self, section: LiteralString, key: LiteralString, *, separator: str = ",") -> list[float]: ...
@@ -341,9 +163,19 @@ class Config(ConfigParser):
         key: LiteralString,
         *,
         separator: str = ",",
-        **kwargs: Sequence[float],
+        fallback: Sequence[float] = __sentinel,
     ) -> list[float]:
-        return get_float_list(self, self._sample_name, section, key, separator=separator, **kwargs)
+        try:
+            return list(
+                map(
+                    float,
+                    self.get(section=section, key=key, fallback=fallback).split(separator),
+                ),
+            )
+        except LookupError:
+            if fallback is not Config.__sentinel:
+                return list(fallback)
+            raise
 
     @overload
     def get_decimal_list(
@@ -370,9 +202,19 @@ class Config(ConfigParser):
         key: LiteralString,
         *,
         separator: str = ",",
-        **kwargs: Sequence[float],
+        fallback: Sequence[float] = __sentinel,
     ) -> list[Decimal]:
-        return get_decimal_list(self, self._sample_name, section, key, separator=separator, **kwargs)
+        try:
+            return list(
+                map(
+                    Decimal,
+                    self.get(section=section, key=key, fallback=fallback).split(separator),
+                ),
+            )
+        except LookupError:
+            if fallback is not Config.__sentinel:
+                return list(map(Decimal, fallback))
+            raise
 
     @overload
     def get_slice_sequence(
@@ -380,8 +222,8 @@ class Config(ConfigParser):
         section: LiteralString,
         key: LiteralString,
         *,
-        slice_separator: str | Sequence[str] = ("..", ":"),
-        items_separator: str | Sequence[str] = (",", ";"),
+        slice_separator: str | Iterable[str] = ("..", ":"),
+        items_separator: str | Iterable[str] = (",", ";"),
     ) -> SliceSequence: ...
 
     @overload
@@ -391,8 +233,8 @@ class Config(ConfigParser):
         key: LiteralString,
         *,
         fallback: SliceSequence,
-        slice_separator: str | Sequence[str] = ("..", ":"),
-        items_separator: str | Sequence[str] = (",", ";"),
+        slice_separator: str | Iterable[str] = ("..", ":"),
+        items_separator: str | Iterable[str] = (",", ";"),
     ) -> SliceSequence: ...
 
     def get_slice_sequence(
@@ -400,16 +242,12 @@ class Config(ConfigParser):
         section: LiteralString,
         key: LiteralString,
         *,
-        slice_separator: str | Sequence[str] = ("..", ":"),
-        items_separator: str | Sequence[str] = (",", ";"),
-        **kwargs: SliceSequence,
+        slice_separator: str | Iterable[str] = ("..", ":"),
+        items_separator: str | Iterable[str] = (",", ";"),
+        fallback: SliceSequence = __sentinel,
     ) -> SliceSequence:
-        return get_slice_sequence(
-            self,
-            self._sample_name,
-            section,
-            key,
+        return SliceSequence(
+            self.get(section=section, key=key, fallback=fallback),
             slice_separator=slice_separator,
             items_separator=items_separator,
-            **kwargs,
         )
